c3d31a051584e2699beccabf115222d61b0bd7a9
Brian <brian.hardy67@gmail.com>
2021-10-14 17:30:40 -0400

    added reqs and framework

--- src/App.js
+++ src/App.js
@@ -23,3 +23,21 @@ function App() {
 }
 
 export default App;
+
+
+/*
+API URL: https://pokeapi.co/api/v2/{endpoint}/
+
+Mandatory Users Stories
+---As a user I:
+[] Want to be presented with a list of the first 10 Pokemon from the series on the main page
+[] Show the details of selected Pokemon in a nested route
+[] Want be able to select Pokemon as “favorites” so that I can access them later.
+[] Want a link with a /favorites route that leads to all of the cards that I have favored.
+
+---Stretch Goals
+[] Paginate the initial list to allow for browsing a larger selection of Pokemon
+[] Search for a Pokemon via its name, retain ability to view details and add it to favorites
+[] Use Material UI to style cards.
+
+*/ 
